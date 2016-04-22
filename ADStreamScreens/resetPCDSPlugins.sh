ac87e3b5a4d7b3bdfcaec1cc734a336486ba0c13
Bruce Hill <bhill@slac.stanford.edu>
2016-04-22 21:22:26 +0000

    Updated the resetPCDSPlugins.sh script, modifified ImagePlugin.edl to hide Bayer widget when in mono mode, and removed some obsolete fixed dimension viewer screens.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/ADStream/current@25210 ececa60f-9f63-0410-97c7-ea05888eb260

--- ADStreamScreens/resetPCDSPlugins.sh
+++ ADStreamScreens/resetPCDSPlugins.sh
@@ -14,15 +14,14 @@ caput ${CAM}:ROI5:EnableScale		0
 caput ${CAM}:ROI5:EnableCallbacks	1
 
 # Reset IMAGE1 plugin
-caput ${CAM}:IMAGE1:NDArrayPort		ROI5
-caput ${CAM}:IMAGE1:MinCallbackTime	0.02
+caput ${CAM}:IMAGE1:NDArrayPort		CAM
+caput ${CAM}:IMAGE1:MinCallbackTime	0.00
 caput ${CAM}:IMAGE1:EnableCallbacks	1
 
 # Reset ROI6 for Binned Image with overlay
-caput ${CAM}:ROI6:Name				IMG_OVER2
+caput ${CAM}:ROI6:Name				"2x2 scaled bin"
 caput ${CAM}:ROI6:NDArrayPort		CAM
 caput ${CAM}:ROI6:MinCallbackTime	0.2
-#caput ${CAM}:ROI6:Scale				64.0
 caput ${CAM}:ROI6:Scale				4.0
 caput ${CAM}:ROI6:BinX				2
 caput ${CAM}:ROI6:BinY				2
@@ -32,24 +31,37 @@ caput ${CAM}:ROI6:EnableScale		1
 caput ${CAM}:ROI6:EnableCallbacks	1
 
 # Reset Over2 for Binned Image with overlay
-caput ${CAM}:Over2:Name				IMG_OVER2
+caput ${CAM}:Over2:Name				"Gr Square"
 caput ${CAM}:Over2:NDArrayPort		ROI6
 caput ${CAM}:Over2:EnableCallbacks	1
+caput ${CAM}:Over2:MinCallbackTime	0.2
 caput ${CAM}:Over2:2:Use			0
 caput ${CAM}:Over2:2:Shape			1
 caput ${CAM}:Over2:2:Green			1023
 
-# Reset IMG_OVER2 BinnedImage plugin
-caput ${CAM}:IMG_OVER2:NDArrayPort		Over2
-caput ${CAM}:IMG_OVER2:MinCallbackTime	0.5
-caput ${CAM}:IMG_OVER2:EnableCallbacks	1
+# See if we have IMG_OVER2
+caget ${CAM}:IMG_OVER2:NDArrayPort 2> /dev/null
+if [ ! $? ]; then
+	# Reset IMG_OVER2 BinnedImage plugin
+	caput ${CAM}:IMG_OVER2:NDArrayPort		Over2
+	caput ${CAM}:IMG_OVER2:MinCallbackTime	0.2
+	caput ${CAM}:IMG_OVER2:EnableCallbacks	1
+fi
+
+# See if we have IMG_OVER2
+caget ${CAM}:IMAGE2:NDArrayPort 2> /dev/null
+	if [ ! $? ]; then
+	# Reset IMAGE2 BinnedImage plugin
+	caput ${CAM}:IMAGE2:NDArrayPort		ROI6
+	caput ${CAM}:IMAGE2:MinCallbackTime	0.2
+	caput ${CAM}:IMG_OVER2:EnableCallbacks	1
+fi
 
 # Reset ROI7 for Thumbnail binning
 caput ${CAM}:ROI7:Name				Thumbnail
 caput ${CAM}:ROI7:NDArrayPort		CAM
-caput ${CAM}:ROI7:MinCallbackTime	0.2
-caput ${CAM}:ROI7:Scale				256.0
-#caput ${CAM}:ROI7:Scale				16.0
+caput ${CAM}:ROI7:MinCallbackTime	1.0
+caput ${CAM}:ROI7:Scale				16.0
 caput ${CAM}:ROI7:BinX				4
 caput ${CAM}:ROI7:BinY				4
 caput ${CAM}:ROI7:EnableX			1
@@ -59,5 +71,5 @@ caput ${CAM}:ROI7:EnableCallbacks	1
 
 # Reset BinnedImage plugin
 caput ${CAM}:THUMBNAIL:NDArrayPort		ROI7
-caput ${CAM}:THUMBNAIL:MinCallbackTime	0.5
+caput ${CAM}:THUMBNAIL:MinCallbackTime	1.0
 caput ${CAM}:THUMBNAIL:EnableCallbacks	1
