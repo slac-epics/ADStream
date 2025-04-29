9dc12c109c6f2e475d074e6b960385d096f82ec0
Bruce Hill <bhill@slac.stanford.edu>
2025-04-29 15:31:57 -0700

    Added MAX_THREADS, MAX_BUFFERS, MAX_MEMORY, PRIORITY, and STACK_SIZE macros to stream configure cmd scripts.

--- streamApp/pluginDb/pluginImage.cmd
+++ streamApp/pluginDb/pluginImage.cmd
@@ -8,7 +8,7 @@
 #-	IMAGE_NELM	- How many elements in the image (x*y for b/w, x*y*3 for color)
 
 #- Configure the plugin
-NDStdArraysConfigure( "$(IMAGE_NAME)", $(QSIZE), 0, "$(PLUGIN_SRC)", 0, -1)
+NDStdArraysConfigure( "$(IMAGE_NAME)", $(QSIZE), 0, "$(PLUGIN_SRC)", 0, 0, 0, 0, 0, $(MAX_THREADS=5) )
 
 #- Load the plugin records
 dbLoadRecords( "db/pluginImage.db",  "CAM=$(CAM_PV),CAM_PORT=$(CAM_PORT),PLUGIN_SRC=$(PLUGIN_SRC),IMAGE_NAME=$(IMAGE_NAME),IMAGE_NELM=$(IMAGE_NELM),IMAGE_FTVL=$(IMAGE_FTVL),IMAGE_TYPE=$(IMAGE_TYPE)" )
